158a587b502ac465fc939fa5dafb10e6c60de75d
maxwell <info@maxwell.lv>
2021-06-02 16:38:15 +0300

    added print only on updates option

--- task1/binance.py
+++ task1/binance.py
@@ -11,8 +11,10 @@ from math import inf
 class PublicBinance(AbstractAsyncApiConsumer):
     exchange = "binance"
 
-    def __init__(self, uri: string):
+    def __init__(self, uri: string, print_only_updates: bool = False):
         self.uri = uri
+        self.print_only_updates = print_only_updates
+        self.updated = 0b00  # first bit -- bid updated, second bit -- ask updated
         self.highest_bid = {"price": -inf, "time": time.time()}
         self.lowest_ask = {"price": inf, "time": time.time()}
         self.loop = asyncio.new_event_loop()
@@ -28,9 +30,14 @@ class PublicBinance(AbstractAsyncApiConsumer):
 
     def _process_prices(self, data):
         best_bid, best_ask = float(data["b"]), float(data["a"])
+        self.updated = 0b00
         self._check_and_update_ask(best_ask)
         self._check_and_update_bid(best_bid)
-        self.print_current_best_prices()
+        if not self.print_only_updates:
+            self.print_current_best_prices()
+        else:
+            if self.updated > 0:  # at least one is updated
+                self.print_current_best_prices()
 
     def print_current_best_prices(self):
         # TODO: consider to move bid/ask to own objects and hide all logic there
@@ -45,11 +52,13 @@ class PublicBinance(AbstractAsyncApiConsumer):
 
     def _check_and_update_ask(self, best_ask: float):
         if best_ask < self.lowest_ask["price"]:
+            self.updated = self.updated | 0b01
             self.lowest_ask["price"] = best_ask
             self.lowest_ask["time"] = time.time()
 
     def _check_and_update_bid(self, best_bid: float):
         if best_bid > self.highest_bid["price"]:
+            self.updated = self.updated | 0b10
             self.highest_bid["price"] = best_bid
             self.highest_bid["time"] = time.time()
 
@@ -70,4 +79,4 @@ if __name__ == "__main__":
     # logging.getLogger().setLevel(logging.DEBUG)
     # TODO: move this to config
     uri = "wss://stream.binance.com:9443/ws/btcusdt@ticker"
-    binance = PublicBinance(uri)
+    binance = PublicBinance(uri, print_only_updates=True)
